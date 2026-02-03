61d7c4d27f6e88f094d7330a798fd83b1f60d547
ConfortiAngelo <confortiangelod@gmail.com>
2026-02-03 19:02:20 -0300

    port validations created

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
-from utils.validators import validator_ip
+from utils.validators import validator_ip , validator_port
 from utils.exceptions import ScannerError
 
 def main():
     print("Inicio del programa")
 
     try:
-        validator_ip("192.2.2.10123")
-        print("IP válida, continuando escaneo...")
+        validator_ip("192.2.2.10")
+        validator_port(80)
     except ScannerError as e:
         print(f"[ERROR] {e}")
         return
--- utils/validators.py
+++ utils/validators.py
@@ -4,8 +4,16 @@ from utils.exceptions import InvalidIPError , InvalidPortError
 def validator_ip(address):
     try:
         ip = ipaddress.ip_address(address)
+        print(f'IP : {ip} valida ')        
     except ValueError:
-        raise InvalidIPError(f'IP invalida : {address}')
+        raise InvalidIPError(f'IP : {address} invalida ')
 
 
+
+def validator_port(port):
+    try:
+        if port > 1 and port < 65535:
+            print(f'Puerto : {port} valido ')
+    except ValueError:
+        raise InvalidPortError(f'Puerto : {port} invalido')
 
